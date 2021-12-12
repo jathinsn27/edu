5e473013e0e08d3c5355c81d82c00eb6b0857404
jathinsn <jatinchess@gmail.com>
2021-12-12 14:32:43 +0530

    particle

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './Header.jsx';
-// import Particle from './particle.js';
+import Particle from './particle.js';
 
 function Home() {
     return (
         <div className='home'>
             <Header />
             <p>Building educhess 🚀 </p>
-            {/* <Particle /> */}
+            <Particle />
         </div>
     )
 }
